9402a13dd95573f34568231a375ffdcd039e0816
Amit Khobragade <amitkhobragde@live.com>
2023-09-01 16:21:48 +0530

    calculator generic numbers

---
+++ CalculatorTask/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTask
+{
+    public static class Calculator
+    {
+        public static INumber<T> Sum<T>(T x, T y) where T : INumber<T>
+		{
+			return x + y;
+		}
+}
+}
